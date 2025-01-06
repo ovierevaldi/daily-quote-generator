ac113dd2ce81ed9e9d306d124a75a7d0ff44e61c
Ovie Revaldi <ovierevaldi@gmail.com>
2025-01-06 13:34:10 +0700

    Add Diphylleia font
    Import the font from google font

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,12 @@ import './App.css'
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div>
+      <h1 className='text-7xl text-center font-diphylleia'>
+        Quote Of The Day
+      </h1>
+
+    </div>
   )
 }
 
